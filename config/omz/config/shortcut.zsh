a90c8de71743728fab617aee483e69f83e38caf0
CH <ch915753811@gmail.com>
2024-03-27 22:36:08 +0800

    日常更新

--- config/omz/config/shortcut.zsh
+++ config/omz/config/shortcut.zsh
@@ -12,8 +12,12 @@ alias 0.5="xrandr --output eDP-1 --brightness 0.5"
 alias 0.8="xrandr --output eDP-1 --brightness 0.8"
 alias 1="xrandr --output eDP-1 --brightness 1"
 alias 0.3="xrandr --output eDP-1 --brightness 0.3"
-alias 144="xrandr --output HDMI-1 --mode 2560x1440 --rate 144 && xrandr --output HDMI-1 --above eDP-1 && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
-alias 75="xrandr --output HDMI-1 --mode 2560x1440 --rate 75 && xrandr --output HDMI-1 --above eDP-1 && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
+
+alias sig="xrandr --output eDP-1 --mode 2560x1600 --pos 0x1440 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off"
+alias ud144="xrandr --output HDMI-1 --mode 2560x1440 --rate 144 && xrandr --output HDMI-1 --above eDP-1 --primary && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
+alias lr144="xrandr --output HDMI-1 --mode 2560x1440 --rate 144 && xrandr --output HDMI-1 --right-of eDP-1 --primary && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
+alias ud75="xrandr --output HDMI-1 --mode 2560x1440 --rate 75 && xrandr --output HDMI-1 --above eDP-1 --primary && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
+alias lr75="xrandr --output HDMI-1 --mode 2560x1440 --rate 75 && xrandr --output HDMI-1 --right-of eDP-1 --primary && feh --randomize --bg-fill ~/Pictures/wallpaper/*.png"
 alias Eng="nvim /home/time/softwares/wudao-dict/wudao-dict/usr/notebook.txt"
 alias vrc="nvim ~/.config/nvim/init.lua"
 alias mus="musicfox"
@@ -27,5 +31,14 @@ alias rm="trash-put"
 alias trashcan="cd ~/.local/share/Trash"
 # 处理解压windows压缩文件的标题乱码问题
 alias unzip="unzip -O CP936"
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+# docker 
+alias dps='sudo docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Ports}}\t{{.Status}}\t{{.Names}}"'
+alias dis='sudo docker images'
+alias dv='sudo docker volume'
+alias d='sudo docker'
 
 
