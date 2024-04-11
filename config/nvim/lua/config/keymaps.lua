2c811f0f1d9b598cfdee42ff9e2e8b77b29508fe
CHEN <915753811@qq.com>
2024-04-11 23:52:22 +0800

    配置优化

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,16 +2,15 @@
 -- 始终设置的默认按键映射: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- 在此添加其他按键映射
 local map = vim.keymap.set
--- insert mode
+
 -- 向右移动光标
 map("i", "<A-a>", "<right>", { desc = "向右移动一格光标", noremap = true })
 -- 复制当前行
 map("i", "<C-d>", "<esc>yyp$a", { desc = "复制当前行", noremap = true })
 -- 退格键快速删除
 map("n", "<bs>", '"_ciw', { desc = "快速删除", noremap = true })
-
 -- 全选
-
+map("n", "<A-a>", "ggVG", { desc = "全选", noremap = true })
 -- Rnvimr插件
 -- 在插入模式下使用 Alt + i 调整 Rnvimr 大小
 vim.api.nvim_set_keymap("t", "<M-i>", "<C-\\><C-n>:RnvimrResize<CR>", { silent = true })
